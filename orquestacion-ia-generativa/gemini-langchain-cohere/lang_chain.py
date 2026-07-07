277567ba3f814c8efbafaac4d68ffd7fa321723c
eduardoalonso250201-maker <eduardo.alonso.250201@gmail.com>
2026-07-06 23:55:28 -0600

    Encadena flujos LCEL para conectar Gemini y Cohere
    - Agrega formato de salida especifico en el system prompt de Gemini
    
    - Integra PromptTemplate para simplificar el prompt de Cohere
    
    - Conecta la salida de la cadena de Gemini como entrada de la cadena de Cohere
    
    - Activa set_debug(True) para ver el paso a paso de la ejecucion

--- orquestacion-ia-generativa/gemini-langchain-cohere/lang_chain.py
+++ orquestacion-ia-generativa/gemini-langchain-cohere/lang_chain.py
@@ -11,40 +11,20 @@ from my_keys import GEMINI_API_KEY, COHERE_API_KEY
 # Utility function that converts an image file into a base64 string
 from my_helper import encode_image
 #we can use the ChatPromptTemplate to create a prompt for the model, this is useful when we want to create a prompt with multiple messages or with variables
-from langchain_core.prompts import ChatPromptTemplate
+#we add a new module "promttemplate", wich enable comunication without the need of using the messages module (system, user, etc)
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 #This library is used to obtain the output of the model in a specific format, for example only the text...
 from langchain_core.output_parsers import StrOutputParser
 
-
-
-
-"""
-# Initialize the Cohere model with the API key
-llm = ChatCohere(
-    cohere_api_key=COHERE_API_KEY
-)
-"""
+#The newt module permit to see each step of the LLM chain
+from langchain_core.globals import set_debug
+set_debug(True)
 
 # Initialize the Google Generative AI model with the API key and model name
 llm = ChatGoogleGenerativeAI(
     api_key=GEMINI_API_KEY,
     model=GEMINI_FLASH
 )
-
-"""
-#here we invoke the gemini model with a simple question, the model will return a response object that contains the content of the answer
-respuesta = llm.invoke("Cuales canales colombianos de youtube me recomiendas para saber mas sobre telefonos inteligentes?")
-print(f"Gemini: ", respuesta.content)
-"""
-
-
-"""
-#here we invoke the cohere model with a simple question, the model will return a response object that contains the content of the answer
-#On the documentation of each API you can find how to call the model, response format, and other parameters 
-respuesta = llm.invoke([HumanMessage(content="Cuales canales colombianos de youtube me recomiendas para saber mas sobre telefonos inteligentes?")])
-print(f"Cohere: ", respuesta.content)
-"""
-
 
 #we can encode the image using the helper function
 imagen = encode_image('datos/ejemplo_grafico.jpg')
@@ -54,11 +34,21 @@ imagen = encode_image('datos/ejemplo_grafico.jpg')
 #with multiple messages or with variables, in this case we are creating a prompt that contains a system message and 
 # a user message with a variable that will be replaced with the base64 string of the image, 
 # this way we can create a prompt that is more flexible and can be reused with different images 
+#note that we add an output format
 template_analisis = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            """Asume que eres analista de imágenes. Tu principal tarea consiste en: analizar una imagen para extraer las informaciones más relevantes de manera objetiva"""
+            """Asume que eres analista de imágenes. 
+            Tu principal tarea consiste en: analizar una imagen para extraer 
+            las informaciones más relevantes de manera objetiva
+            
+            #FORMATO DE SALIDA 
+            Descripcion de la imagen: Tu descripción de la imagen aqui.
+            Etiquetas: Una lista con tres palabras clave separadas con coma.
+            """
+
+            
         ),
         (
             "user",
@@ -84,3 +74,28 @@ cadena_analisis = template_analisis | llm | StrOutputParser()
 respuesta_analisis = cadena_analisis.invoke({"imagen_informada": imagen})
 
 print(respuesta_analisis)
+
+#We are going to create a new chain to connect two LLMs
+
+#we create a new template for the response
+#This template or promt will receive the output of the first model
+template_respuesta = PromptTemplate(
+    template = """
+                Genera un resumen, utilizando un lenguaje claro y objetivo, enfocado en el publico colombiano.
+                La idea es que la comunicacion del resultado sea lo mas sencilla posible, priorizando los registros
+                para consultas posteriores.
+
+                #RESULTADO DE LA IMAGEN
+                {respuesta_analisis_imagen}
+               """,
+               input_variables=["respuesta_analisis_imagen"]
+)
+
+# Initialize the Cohere model with the API key
+llm_cohere = ChatCohere(cohere_api_key=COHERE_API_KEY)
+
+cadena_resumen = template_respuesta | llm_cohere | StrOutputParser()
+
+cadena_compuesta = (cadena_analisis | cadena_resumen)
+
+respuesta = cadena_compuesta.invoke({"imagen_informada":imagen})
